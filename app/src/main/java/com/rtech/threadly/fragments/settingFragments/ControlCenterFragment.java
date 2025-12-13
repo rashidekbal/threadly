a76b8e9b4d56bf8a5b1fef1e06f7623b7aae6c19
Rasid Ekbal <rtechdevlopment123@gmail.com>
2025-12-13 22:23:59 +0530

    add account control center

---
+++ app/src/main/java/com/rtech/threadly/fragments/settingFragments/ControlCenterFragment.java
@@ -0,0 +1,43 @@
+package com.rtech.threadly.fragments.settingFragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rtech.threadly.R;
+import com.rtech.threadly.databinding.FragmentControlCenterBinding;
+import com.rtech.threadly.models.Profile_Model;
+import com.rtech.threadly.viewmodels.ProfileViewModel;
+
+
+public class ControlCenterFragment extends Fragment {
+    FragmentControlCenterBinding mainXml;
+    private  ProfileViewModel profileViewModel;
+    private Profile_Model  userData;
+
+
+    public ControlCenterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mainXml=FragmentControlCenterBinding.inflate(inflater,container,false);
+        init();
+        return mainXml.getRoot();
+    }
+    private void init (){
+        profileViewModel=new ViewModelProvider(requireActivity()).get(ProfileViewModel.class);
+        profileViewModel.getProfileLiveData().observe(requireActivity(),profileModel -> {
+            userData=profileModel;
+        });
+    }
+}
